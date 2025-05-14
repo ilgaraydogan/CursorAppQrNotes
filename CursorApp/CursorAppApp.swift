3dfaf43932c91348c1bef0bff5bb3f8e6ff7300c
Bayram Ilgar Aydogan <ilgaraydogan@icloud.com>
2025-05-14 16:05:02 +0300

    Initial Commit

---
+++ CursorApp/CursorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorAppApp.swift
+//  CursorApp
+//
+//  Created by Bayram Ilgar Aydoğan on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CursorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
